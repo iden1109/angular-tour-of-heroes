9927bb8510f77e8d2480502f513b12acbb71491d
Iden Liu <idenliu@micron.com>
2018-06-25 16:30:33 +0800

    add http call

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -10,10 +10,10 @@ import { HeroService } from '../hero.service';
 })
 export class HerosComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'windstrom'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'windstrom'
+  // };
 
   heroes: Hero[];
 
@@ -33,5 +33,16 @@ export class HerosComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
+  }
 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
